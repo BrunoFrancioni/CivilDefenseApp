dc2ee60c82ef8909a933b382c36e9ac9d73da9a3
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-10-18 16:27:49 -0300

    Mongoose al proyecto.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntitiesController } from './entities/entities.controller';
+import { EntitiesService } from './entities/entities.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/civildefense')],
+  controllers: [AppController, EntitiesController],
+  providers: [AppService, EntitiesService],
 })
 export class AppModule {}
