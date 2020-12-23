c5ddac9fadd3dd19e697e4dfd8be7e565093e882
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-12-22 21:43:17 -0300

    Credentials module, controller, service and schema created.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EntitiesModule } from './entities/entities.module';
+import { CredentialsModule } from './credentials/credentials.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@civil-defense.r981f.mongodb.net/civil-defense?retryWrites=true&w=majority`, { useNewUrlParser: true }),
-    EntitiesModule
+    EntitiesModule,
+    CredentialsModule
   ],
   controllers: [AppController],
   providers: [AppService],
