b802166df70cf575534a1b6bb22b144981d8f37f
Bruno Francioni <brunofrancioni1998@gmail.com>
2021-05-22 19:19:34 -0300

    App

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -8,27 +8,29 @@ import Header from './components/shared/Header/Header';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/main/Home/Home';
 import Users from './components/main/Users/Users';
+import Entities from './components/main/Entities/Entities';
 
 function App() {
   return (
-    <>
+    <div className="main-container">
       <Header />
 
       <Container>
         <Row>
-          <Col md={3}>
+          <Col md={2}>
             <Sidebar />
           </Col>
 
           <Col>
             <Router>
               <Route path="/users" component={Users} />
+              <Route path="/entities" component={Entities} />
               <Route exact path="/" component={Home} />
             </Router>
           </Col>
         </Row>
       </Container>
-    </>
+    </div>
   );
 }
 
