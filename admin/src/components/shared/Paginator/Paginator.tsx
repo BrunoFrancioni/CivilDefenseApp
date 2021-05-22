7734985e54d8c469ab9663006a69d386418bb150
Bruno Francioni <brunofrancioni1998@gmail.com>
2021-05-22 19:18:46 -0300

    Paginator finished

--- admin/src/components/shared/Paginator/Paginator.tsx
+++ admin/src/components/shared/Paginator/Paginator.tsx
@@ -23,28 +23,66 @@ class Paginator extends React.Component<PaginatorProps, PaginatorState, {}> {
             totalPages = 1;
         } else if (this.props.totalResults % this.props.sizePage == 0) {
             totalPages = this.props.totalResults / this.props.sizePage;
-            items.push(<Pagination.First />);
+            items.push(<Pagination.First key={0} onClick={() => this.changePage(1)} />);
         } else {
-            totalPages = (this.props.totalResults / this.props.sizePage) + 1;
-            items.push(<Pagination.First />);
+            totalPages = (this.props.totalResults / this.props.sizePage) + 0.5;
+            items.push(<Pagination.First key={0} onClick={() => this.changePage(1)} />);
         }
 
-        for (let i = 1; i <= totalPages; i++) {
-            items.push(
-                <Pagination.Item
-                    key={i}
-                    active={this.props.active == i}
-                    onClick={() => this.changePage(i)}
-                    className="hover-item"
-                >{i}</Pagination.Item>
-            );
+        if (totalPages > 5) {
+            if (this.props.active === 1 || this.props.active === 2) {
+                for (let i = 1; i <= 5; i++) {
+                    items.push(
+                        <Pagination.Item
+                            key={i}
+                            active={this.props.active == i}
+                            onClick={() => this.changePage(i)}
+                            className="hover-item"
+                        >{i}</Pagination.Item>
+                    );
+                }
+            } else if (this.props.active === totalPages ||
+                this.props.active === (totalPages - 1)) {
+                for (let i = totalPages - 5; i <= totalPages; i++) {
+                    items.push(
+                        <Pagination.Item
+                            key={i}
+                            active={this.props.active == i}
+                            onClick={() => this.changePage(i)}
+                            className="hover-item"
+                        >{i}</Pagination.Item>
+                    );
+                }
+            } else {
+                for (let i = this.props.active - 2; i <= this.props.active + 2; i++) {
+                    items.push(
+                        <Pagination.Item
+                            key={i}
+                            active={this.props.active == i}
+                            onClick={() => this.changePage(i)}
+                            className="hover-item"
+                        >{i}</Pagination.Item>
+                    );
+                }
+            }
+        } else {
+            for (let i = 1; i <= totalPages; i++) {
+                items.push(
+                    <Pagination.Item
+                        key={i}
+                        active={this.props.active == i}
+                        onClick={() => this.changePage(i)}
+                        className="hover-item"
+                    >{i}</Pagination.Item>
+                );
+            }
         }
 
         if (this.props.totalResults >= this.props.sizePage) {
             if (this.props.totalResults % this.props.sizePage == 0) {
-                items.push(<Pagination.Last />);
+                items.push(<Pagination.Last key={totalPages + 1} onClick={() => this.changePage(totalPages)} />);
             } else {
-                items.push(<Pagination.Last />);
+                items.push(<Pagination.Last key={totalPages + 1} onClick={() => this.changePage(totalPages)} />);
             }
         }
 
