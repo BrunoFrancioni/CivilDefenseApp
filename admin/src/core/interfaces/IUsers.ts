d258956cc8ad5b9b91862fa2c90aae58cc45366e
Bruno Francioni <brunofrancioni1998@gmail.com>
2021-05-28 20:00:46 -0300

    Users interfaces

---
+++ admin/src/core/interfaces/IUsers.ts
@@ -0,0 +1,46 @@
+export interface ICredential {
+    _id: string;
+    name_lastname: string;
+    dni: string;
+    organization: string;
+    email: string;
+}
+
+export interface ICreateCredentials {
+    name_lastname: string;
+    dni: string;
+    organization: string;
+    email: string;
+}
+
+export interface ICreateCredentialsResult {
+    result: boolean;
+    pass: string;
+}
+
+export interface ILoginCredential {
+    email: string;
+    password: string;
+}
+
+export interface ILoginResult {
+    result: boolean;
+    token: string;
+    user: ICredential;
+}
+
+export interface IGetCredentials {
+    page: number;
+    size: number;
+}
+
+export interface IEditCredential {
+    name_lastname: string;
+    dni: string;
+    organization: string;
+}
+
+export interface IGetNewPassword {
+    idAdmin: string;
+    idCredential: string;
+}
