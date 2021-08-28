a8e0bd769d175038241916b6faee2e8c16a6b80e
Bruno Francioni <brunofrancioni1998@gmail.com>
2021-08-28 17:46:20 -0300

    Entities Service fixed

--- app/src/core/services/EntitiesService.ts
+++ app/src/core/services/EntitiesService.ts
@@ -4,10 +4,8 @@ class EntitiesService {
     private path: string = '/entities';
 
     public async getEntities() {
-        return await API.get(this.path);
+        return await API.get(this.path + '/all');
     }
-
-
 }
 
 export default EntitiesService;
